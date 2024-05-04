a8e5dbe0d8c2b2027b7f874f949445796873782f
crhnf549 <117624726+crhnf549@users.noreply.github.com>
2024-05-05 05:24:07 +0900

    Update line_bot.py

--- line_bot.py
+++ line_bot.py
@@ -16,6 +16,7 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+history = []
 
 @app.route("/", methods=['POST'])
 def callback():
