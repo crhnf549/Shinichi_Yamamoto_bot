c82e253b9c57476144c71e68f88b712bdb6946c3
crhnf549 <117624726+crhnf549@users.noreply.github.com>
2024-05-05 22:12:27 +0900

    Update line_broadcast.py

--- line_broadcast.py
+++ line_broadcast.py
@@ -1,6 +1,7 @@
 import time
 import os
-from linebot.v3.messaging import MessagingApi
+#from linebot.v3.messaging import MessagingApi
+from linebot.v3.messaging import Configuration, MessagingApi, ApiClient, PushMessageRequest, ApiException
 from linebot.models import TextSendMessage
 from apscheduler.schedulers.background import BackgroundScheduler
 from Shinichi_Yamamoto_bot import chat
@@ -10,9 +11,20 @@ import sys
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
-
+USER_ID = "U812711723bfbfcd7047ba3d7bd89e717"
 line_bot_api = MessagingApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+configuration = Configuration(access_token = os.getenv('CHANNEL_ACCESS_TOKEN'))
 scheduler = BackgroundScheduler()
+
+message_dict = {
+    'to': USER_ID,
+    'messages': [
+        {
+            'type': 'text',
+            'text': 'テストメッセージ'
+        },
+    ]
+}
 
 # 毎日特定の時間に実行されるジョブ
 def send_message():
@@ -20,7 +32,16 @@ def send_message():
     everyday_words = "---今日の励ましの一言---\n" + answer + "\n\n※毎日自動配信しています。"
     print(everyday_words)
     #line_bot_api.broadcast(TextSendMessage(text=everyday_words))
-    line_bot_api.push_message("U812711723bfbfcd7047ba3d7bd89e717", TextSendMessage(text="テスト中"))
+    #line_bot_api.push_message("U812711723bfbfcd7047ba3d7bd89e717", TextSendMessage(text="テスト中"))
+    with ApiClient(configuration) as api_client:
+        # Create an instance of the API class
+        line_bot_api = MessagingApi(api_client)
+        push_message_request = PushMessageRequest.from_dict(message_dict)
+        try:
+            push_message_result = line_bot_api.push_message_with_http_info(push_message_request, _return_http_data_only=False)
+            print(f'The response of MessagingApi->push_message status code => {push_message_result.status_code}')
+        except ApiException as e:
+            print('Exception when calling MessagingApi->push_message: %s\n' % e)
 
 def every_minites_task():
     current_time = time.time()
