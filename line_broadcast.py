374b8548b7b668a27ef7404e8764e4515cd09359
crhnf549 <117624726+crhnf549@users.noreply.github.com>
2024-05-05 22:42:51 +0900

    Update line_broadcast.py

--- line_broadcast.py
+++ line_broadcast.py
@@ -22,7 +22,7 @@ def send_message():
     everyday_words = "---今日の励ましの一言---\n" + answer + "\n\n※毎日自動配信しています。"
     print(everyday_words)
     message_dict = {
-        'to': USER_ID,
+        #'to': USER_ID,
         'messages': [
             {
                 'type': 'text',
@@ -32,6 +32,7 @@ def send_message():
     }
     #line_bot_api.broadcast(TextSendMessage(text=everyday_words))
     #line_bot_api.push_message("U812711723bfbfcd7047ba3d7bd89e717", TextSendMessage(text="テスト中"))
+    '''
     with ApiClient(configuration) as api_client:
         # Create an instance of the API class
         line_bot_api = MessagingApi(api_client)
@@ -41,6 +42,17 @@ def send_message():
             print(f'The response of MessagingApi->push_message status code => {push_message_result.status_code}')
         except ApiException as e:
             print('Exception when calling MessagingApi->push_message: %s\n' % e)
+    '''
+    with ApiClient(configuration) as api_client:
+        # Create an instance of the API class
+        line_bot_api = MessagingApi(api_client)
+        broadcast_message_request = BroadcastMessageRequest.from_dict(message_dict)
+        try:
+            broadcast_message_result = line_bot_api.broadcast_message_with_http_info(broadcast_message_request, _return_http_data_only=False)
+            print(f'The response of MessagingApi->broadcast_message status code => {broadcast_message_result.status_code}')
+        except ApiException as e:
+            print('Exception when calling MessagingApi->broadcast_message: %s\n' % e)
+
 
 def every_minites_task():
     current_time = time.time()
@@ -49,7 +61,7 @@ def every_minites_task():
 
 if __name__ == "__main__":
     # ジョブをスケジュールする
-    scheduler.add_job(send_message, 'cron', minute='*')
+    #scheduler.add_job(send_message, 'cron', minute='*')
     scheduler.add_job(every_minites_task, 'cron', minute='*')
     scheduler.add_job(send_message, 'cron', hour=0)
 
