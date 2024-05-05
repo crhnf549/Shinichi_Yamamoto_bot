ef5e5d716a3e90712389738bc8f3244f2bce6164
crhnf549 <117624726+crhnf549@users.noreply.github.com>
2024-05-05 21:11:33 +0900

    Update line_broadcast.py

--- line_broadcast.py
+++ line_broadcast.py
@@ -4,13 +4,19 @@ from linebot.v3.messaging import MessagingApi
 from linebot.models import TextSendMessage
 from apscheduler.schedulers.background import BackgroundScheduler
 from Shinichi_Yamamoto_bot import chat
+import io
+import sys
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
 
 line_bot_api = MessagingApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 scheduler = BackgroundScheduler()
 
 # 毎日特定の時間に実行されるジョブ
 def send_message():
-    answer, id = chat("大聖人の御書や、御聖訓、偉人の言葉を引用し、あなたの解釈とともに、悩んでいる人、苦しんでいる人へ励ましの言葉を書いて。絶対にすべて日本語で書くこと。", "crhnf549", "")
+    answer, id = chat("大聖人の御書や、御聖訓、偉人の言葉を一つだけ引用し、あなたの解釈とともに、励ましの言葉を書いて。絶対にすべて日本語で書くこと。", "crhnf549", "")
     everyday_words = "---今日の励ましの一言---\n" + answer + "\n\n※毎日自動配信しています。"
     print(everyday_words)
     #line_bot_api.broadcast(TextSendMessage(text=everyday_words))
